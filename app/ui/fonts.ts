816f97e80e383453de16ffe1b441a823ec0ce46c
nicoloprog <nicosubywrx@gmail.com>
2025-04-25 01:24:22 -0400

    fixing shit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,13 @@
-import { Inter, Encode_Sans } from "next/font/google";
+import { Inter, Lusitana, Encode_Sans } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
 export const encodeSans = Encode_Sans({
   weight: ["400", "700"], // add or change weights as needed
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
   subsets: ["latin"],
 });
